890f9596f64cc98f6c601632e11fef6ac0ed6308
Dustin Frisch <dustin@dustin-frisch.de>
2011-05-15 19:09:34 +0200

    GPL haeders added to renderers

--- renderers/creole.py
+++ renderers/creole.py
@@ -1,3 +1,18 @@
+# This file is part of PyCo.
+# 
+# PyCo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PyCo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with PyCo.  If not, see <http://www.gnu.org/licenses/>.
+
 # The page content is written using creole syntax. the creole syntax is the
 # commonly known as wiki-syntax. This allows to write and edit page content using
 # every text editor in an easy to learn syntax. For further information about
